8a15e231925c7136c46b6aa5ca07d41272eb47bf
bcornec44 <58662451+bcornec44@users.noreply.github.com>
2024-05-10 12:41:17 +0200

    trying to get a mainwindow working

--- NgFront/SocialNetAdvisorNgFront/src/app/app.module.ts
+++ NgFront/SocialNetAdvisorNgFront/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainWindowComponent } from './main-window/main-window.component';
+import { SafeUrlPipe } from './safe-url.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainWindowComponent,
+    SafeUrlPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ NgFront/SocialNetAdvisorNgFront/src/app/main-window/main-window.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-window',
+  templateUrl: './main-window.component.html',
+  styleUrls: ['./main-window.component.css']
+})
+export class MainWindowComponent {
+  public url: string | undefined;
+  webViewUrl: string;
+  showSuggestions: boolean = false;
+
+  constructor() { }
+
+  suggest() {
+    // Implement suggestion logic
+  }
+
+  home() {
+    // Logic to handle home command
+  }
+
+  goToUrl() {
+    this.webViewUrl = this.url;
+  }
+
+  goTo(url: string) {
+    this.webViewUrl = url;
+  }
+}
